cbdf059e07e7d3e9d7e8120bfb852c6a6860304b
jumson <munsonj@gmail.com>
2018-02-11 16:12:40 -0800

    command line version update
    node vidDown-c.js file.har .mp4

--- README.md
+++ README.md
@@ -11,20 +11,29 @@ These scripts parse out a .har file for a particular media type.
 It creates a csv that lists the media links.
 Then it downloads the files.
 
+
 Tested with 12 videos at once at skillport.com (used media='.mp4')
 
 Tested twice with 25+ video lessons at lynda.com (used media='mp4') (these all dowload simultaneously -- and I did not get booted...yet..)
 
 Did not get it to work with youtube.
 
+Works with Udemy -- except I tried to do 30 videos at once and the HAR file was over 400M. The halParser failed "Error: "toString()" failed". But smaller files work (60MB...)
+
 To use the scripts, I simply place them in the same directory as the HAR file, open a command window:
 ```
-C:\Users\user\Downloads\lynda\course1> node
+C:\Users\user\Downloads\lynda\course1>node
 > var vd = require('./vidDown.js')
 > vd('lynda.har','mp4')
 ```
+
+for the command line version -- vidDown-c.js
+```
+C:\Users\user\Downloads\lynda\course1>node lynda.har .mp4
+```
+
 One consideration is that i'm downloading these in the same time period that my credentials did not expire. I am not confident this will work if I wait too long after gathering the HAR file.
 
-**update** udemy does not have unique names for their mp4 files, therfore, I'm now adding a unique number to each file-name as a sequence.
+**update** udemy does not have unique names for their mp4 files, therfore, I added a unique number to each file-name as a sequence.
 
 The problem was that nodeJS was simply writing all over the same file and creating an unreadable video.
---
+++ vidDown-c.js
@@ -0,0 +1,45 @@
+const harParse = require('./harParser.js'); // harParser(INPUTFILE,OUTPUTFILE,MEDIA) -- strings
+const https = require('https');
+const fs = require('fs');
+const rl = require('readline');
+var url = require("url");
+var path = require("path");
+//this takes the hal file, input file and media indicators as input, same as har parser.
+//it creates its own output file based on input filename
+//then it runs harParse and uses that output file as input to download the files
+const inF = process.argv[2];
+const media = process.argv[3];
+var outF = inF + '.csv';
+try {
+    var count = harParse(inF,outF,media);
+    var current = 0;
+    if(count != 0){
+        var lineReader = rl.createInterface({
+            input: fs.createReadStream(outF)
+            });
+        lineReader.on('line', function (line) {
+            //parse out file name to make output filename
+            var outFile = `${current.toString()}_${getFileName(line)}`; //should be #_file.mp4
+            var writeStream = fs.createWriteStream(outFile);
+            var request = https.get(line, function(response) {
+                response.pipe(writeStream);
+            });
+            current++;
+            console.log(`downloaded: ${current} of ${count}`);
+        });
+    } else {
+        console.log('harParser failed');
+        return 0;
+    }
+} catch (e) {
+    console.log(`broke, error: ${e}`);
+    return 0;
+}
+return 1;
+
+
+function getFileName(line){
+    var parsed = url.parse(line);
+    var lastPart = path.basename(parsed.pathname);
+    return lastPart;
+}
